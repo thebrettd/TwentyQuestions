1f8b22705a4b1a001eda75137abc8b3e1add669c
unknown <brett@BDavis-D2.xtime.com>
2012-05-17 14:02:11 -0700

    Commit rubymine defaults

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwentyQuestions::Application.config.secret_token = 'e19776b2f4145f6e7fecc316142a290cbbc0872b8926ee9a0fabdaacfd9d7d8ea668faf6de744f71f7520c80a8882aee161f8772331cd1c2bdc4cdabe97e8de5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwentyQuestions::Application.config.session_store :cookie_store, key: '_TwentyQuestions_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwentyQuestions::Application.config.session_store :active_record_store
