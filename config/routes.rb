012125ba8263a97df3118a09633a7b8ba3dbcb02
Brett Davis <thebrettd@gmail.com>
2012-05-17 14:30:32 -0700

    Commit genreated scaffolding for Game,Question,User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 TwentyQuestions::Application.routes.draw do
+  resources :users
+
+  resources :questions
+
+  resources :games
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
